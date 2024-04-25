9600e88fcf46b713eef979899e4fbe7df40a89a8
LucyTuan <yztuan1129@gmail.com>
2024-04-25 16:50:40 +0800

    ♻️ [Refactor] auto_pad in layer_helper.py

--- tools/layer_helper.py
+++ tools/layer_helper.py
@@ -5,8 +5,28 @@ import torch.nn as nn
 from model import module
 
 
-def auto_pad():
-    raise NotImplementedError
+def auto_pad(kernel_size, dilation):
+    """
+    Calculate padding size for convolutional operations given kernel size and dilation.
+
+    :param kernel_size: An int or tuple of two ints indicating the kernel dimensions.
+    :param dilation: An int or tuple of two ints indicating the dilation in height and width.
+    :return: An int or tuple of ints representing the padding required.
+    """
+
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size, kernel_size)
+    if isinstance(dilation, int):
+        dilation = (dilation, dilation)
+
+    effective_kernel_h = dilation[0] * (kernel_size[0] - 1) + 1
+    effective_kernel_w = dilation[1] * (kernel_size[1] - 1) + 1
+    padding_h = effective_kernel_h // 2
+    padding_w = effective_kernel_w // 2
+
+    if padding_h == padding_w:
+        return padding_h
+    return (padding_h, padding_w)
 
 
 def get_layer_map():
