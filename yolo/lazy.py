d58a9b6734ffc1e2b532a21f37bf2c52f53f8993
henrytsui000 <henrytsui000@gmail.com>
2024-06-09 01:04:04 +0800

    :truck: [Rename] Tracker to Logger, handle all log

--- yolo/lazy.py
+++ yolo/lazy.py
@@ -13,13 +13,12 @@ from yolo.tools.data_loader import create_dataloader
 from yolo.tools.solver import ModelTester, ModelTrainer
 from yolo.utils.bounding_box_utils import Vec2Box
 from yolo.utils.deploy_utils import FastModelLoader
-from yolo.utils.logging_utils import custom_logger, validate_log_directory
+from yolo.utils.logging_utils import ProgressLogger
 
 
 @hydra.main(config_path="config", config_name="config", version_base=None)
 def main(cfg: Config):
-    custom_logger()
-    save_path = validate_log_directory(cfg, exp_name=cfg.name)
+    progress = ProgressLogger(cfg, exp_name=cfg.name)
     dataloader = create_dataloader(cfg.task.data, cfg.dataset, cfg.task.task)
     device = torch.device(cfg.device)
     if getattr(cfg.task, "fast_inference", False):
@@ -31,11 +30,11 @@ def main(cfg: Config):
     vec2box = Vec2Box(model, cfg.image_size, device)
 
     if cfg.task.task == "train":
-        trainer = ModelTrainer(cfg, model, vec2box, save_path, device)
+        trainer = ModelTrainer(cfg, model, vec2box, progress, device)
         trainer.solve(dataloader)
 
     if cfg.task.task == "inference":
-        tester = ModelTester(cfg, model, vec2box, save_path, device)
+        tester = ModelTester(cfg, model, vec2box, progress, device)
         tester.solve(dataloader)
 
 
