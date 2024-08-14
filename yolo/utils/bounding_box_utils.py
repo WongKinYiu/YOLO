32417ddbec547ab653cb533552d1230e943e1afd
f-fl0 <florent@seqsense.com>
2024-08-14 12:54:11 +0900

    Use valid_con instead of valid_cls in batched_nms

--- yolo/utils/bounding_box_utils.py
+++ yolo/utils/bounding_box_utils.py
@@ -387,7 +387,7 @@ def bbox_nms(cls_dist: Tensor, bbox: Tensor, nms_cfg: NMSConfig, confidence: Opt
     valid_box = bbox[valid_mask.repeat(1, 1, 4)].view(-1, 4)
 
     batch_idx, *_ = torch.where(valid_mask)
-    nms_idx = batched_nms(valid_box, valid_cls, batch_idx, nms_cfg.min_iou)
+    nms_idx = batched_nms(valid_box, valid_con, batch_idx, nms_cfg.min_iou)
     predicts_nms = []
     for idx in range(cls_dist.size(0)):
         instance_idx = nms_idx[idx == batch_idx[nms_idx]]
