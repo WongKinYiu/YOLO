d44dbc0e5c68a82665c529a52de7c2f5e3853e37
henrytsui000 <henrytsui000@gmail.com>
2024-08-23 15:23:57 +0800

    ✨ [New] Add a classification head!

--- yolo/model/module.py
+++ yolo/model/module.py
@@ -130,6 +130,7 @@ class MultiheadDetection(nn.Module):
         return [head(x) for x, head in zip(x_list, self.heads)]
 
 
+# ----------- Segmentation Class ----------- #
 class Segmentation(nn.Module):
     def __init__(self, in_channels: Tuple[int], num_maskes: int):
         super().__init__()
@@ -174,6 +175,20 @@ class Anchor2Vec(nn.Module):
         vector_x = anchor_x.softmax(dim=1)
         vector_x = self.anc2vec(vector_x)[:, 0]
         return anchor_x, vector_x
+
+
+# ----------- Classification Class ----------- #
+class Classification(nn.Module):
+    def __init__(self, in_channel: int, num_classes: int, *, neck_channels=1024, **head_args):
+        super().__init__()
+        self.conv = Conv(in_channel, neck_channels, 1)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.head = nn.Linear(neck_channels, num_classes)
+
+    def forward(self, x: Tensor) -> Tuple[Tensor]:
+        x = self.pool(self.conv(x))
+        x = self.head(x.flatten(start_dim=1))
+        return x
 
 
 # ----------- Backbone Class ----------- #
