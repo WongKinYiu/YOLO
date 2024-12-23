ee709f5f9e882df421384393be273e5fceedd42d
henrytsui000 <henrytsui000@gmail.com>
2024-12-23 15:07:57 +0800

    🔨 [Update] gradient clip strategy

--- yolo/lazy.py
+++ yolo/lazy.py
@@ -24,6 +24,7 @@ def main(cfg: Config):
         logger=loggers,
         log_every_n_steps=1,
         gradient_clip_val=10,
+        gradient_clip_algorithm="value",
         deterministic=True,
         enable_progress_bar=not getattr(cfg, "quite", False),
         default_root_dir=save_path,
